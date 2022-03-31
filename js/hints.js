efc34bde5478d5808adcd13b13ea60c11bf7142f
Frank3TheTank3 <francisnic90@gmail.com>
2022-03-31 10:25:24 +0200

    FrancisMainSync

---
+++ js/hints.js
@@ -0,0 +1,13 @@
+function FiftyFitfyJoker()
+{
+
+    let QuestionNumber = document.getElementById('questNum').innerHTML;
+
+    var HintToShow = new Array ( );
+
+    HintToShow[1] = '';
+    HintToShow[1] = '';
+    
+
+
+}
